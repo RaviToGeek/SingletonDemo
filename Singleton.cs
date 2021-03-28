88863db7d622fc7f063da7967e74c69f7ec2f643
Ravi Bhalala <Ravi.Bhalala@quest.com>
2021-03-28 09:41:59 -0400

    Add project files.

---
+++ Singleton.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SingletonDemo
+{
+    public class Singleton
+    {
+        public void printMessage(string message)
+        {
+            Console.WriteLine(message);
+
+        }
+    }
+}
